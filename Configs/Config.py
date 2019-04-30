c2d2521569a6fef2bbb6898207600da9729505de
L1nf3ng <nuaa_llf@126.com>
2019-04-30 17:32:03 +0800

    DAY3，现在开始封装、引入日志功能!

---
+++ Configs/Config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+'''
+@author: d00ms(--)
+@file: config.py
+@time: 2019-4-30 15:47
+@desc: some base configurations
+'''
+
+DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+DEFAULT_ENCODING = 'utf-8'
--- Http/Url.py
+++ Http/Url.py
@@ -8,5 +8,30 @@
 @desc: 封装URL类，主要补全缺省url
 '''
 
+
+from Configs import Config
+
+from urllib.parse import  urlparse
+
 class Url:
-    pass
+    def __init__(self, urlString,encoding=Config.DEFAULT_ENCODING):
+        # 先补齐协议名、端口名
+        if not urlString.startswith('http://') and not urlString.startswith('https://'):
+            urlString = 'http://'+urlString
+        result = urlparse(urlString)
+        self._host = result.hostname
+        self._path = result.path
+        if result.port is None:
+            self._port= 80 if result.scheme=='http' else 443
+        self._params = result.params
+        self._filename = result.path[result.path.index('/')+1:]
+        self._file_ext = result.path[result.path.index('.')+1:]
+
+    def __str__(self):
+        return "the 6-elements tuple is: %s %s %s %s %s %s"%(self._host, str(self._port), self._path, self._filename, self._file_ext, self._params)
+
+
+if __name__=='__main__':
+    # some tests for url class
+    t_url = 'http://www.goodle.com/index.pphjp?a=2134&rb=414#fdfo'
+    print(Url(t_url))
